db37065759db487af505f8b012ed0b47cd218ccd
podics <podics>
2021-06-15 08:29:36 +0000

    testing actions
    as

---
+++ test.sql
@@ -0,0 +1,13 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC This is my Test notebook!
+
+-- COMMAND ----------
+
+-- MAGIC %sql
+-- MAGIC SELECT *
+-- MAGIC FROM csv.`/databricks-datasets/asa/airlines/2008.csv`
+
+-- COMMAND ----------
+
+
